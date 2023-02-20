2a455dc92a969cb519c39d0aa19414c5800f4778
Seshabrahmam Modepalli <Seshabrahmam.Modepalli@omnitracs.com>
2023-02-20 16:19:09 +0530

    shell commands for conditions

---
+++ 06-conditions.sh
@@ -0,0 +1,12 @@
+number = $1
+string = $2
+
+if [ "${number}" -eq 5 ]; then 
+    echo number is 5
+fi
+
+if [ "${string}" -eq abc ]; then 
+    echo string is abc
+else
+    echo string is not abc
+fi
