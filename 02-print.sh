1708dec1272a932ecd632927707f39555027aba1
Seshabrahmam Modepalli <Seshabrahmam.Modepalli@omnitracs.com>
2023-02-20 15:31:32 +0530

    added shell print commands

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,18 @@
 echo Hello world
+## Color Syntax
+# echo -e "\e[COLm MESSAGE \e[0m"
+# echo -e -> Enable colors from echo command
+# \e[COLm -> Enable a Particular Color
+# Color         Codes
+# Red           31
+# Green         32
+# Yellow        33
+# Blue          34
+# Magenta       35
+# Cyan          36
+
+# \e[0m -> Zero code is going to reset the color, Meaning if we enable color for sure we need to disable it, otherwise the color will continue on screen.
+# We can use double or single quotes, However double quotes are preferred.
+
 echo -e "\e[31m WELCOME \e[0m"
 echo Hello World
